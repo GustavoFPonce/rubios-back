39eaae67b7bc5e9a5efb38c6f6d44f1f4d05bed2
Antonella Fanari <antonellafanari94@gmail.com>
2023-08-25 15:05:17 -0300

    fixes module report

--- src/report/report.module.ts
+++ src/report/report.module.ts
@@ -3,9 +3,10 @@ import { ReportController } from './report.controller';
 import { ReportService } from './report.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Credit } from 'src/credit/entities/credit.entity';
+import { PaymentDetail } from 'src/credit/entities/payment-detail.entity';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Credit])],
+  imports:[TypeOrmModule.forFeature([Credit, PaymentDetail])],
   controllers: [ReportController],
   providers: [ReportService],
 })
